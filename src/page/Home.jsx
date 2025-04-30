9396c0420123b0c5a44c712bd71a7abbefbf2c87
Kyaa-A <asnaripacalna@gmail.com>
2025-04-30 12:26:15 +0800

    Side Nav

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { ThemeToggle } from "../components/ThemeToggle";
 import { StarBackground } from "@/components/StarBackground";
+import { Navbar } from "../components/Navbar";
 
 export const Home = () => {
   return (
@@ -12,6 +13,7 @@ export const Home = () => {
       <StarBackground />
 
       {/* Navbar */}
+      <Navbar />
 
       {/* Main Content */}
 
